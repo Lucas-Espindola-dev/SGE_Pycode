be682a858a3656a5384fc6bf678c960308230e35
Lucas Espindola <lucasespindola.dev@gmail.com>
2025-06-10 09:41:36 -0300

    first commit

---
+++ inflows/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from suppliers.models import Supplier
+from products.models import Product
+
+
+class Inflow(models.Model):
+    supplier = models.ForeignKey(Supplier, on_delete=models.PROTECT, related_name='inflows')
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='inflows')
+    quantity = models.IntegerField()
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return str(
+            self.product)  # Transforma-se em str por conta que, se não houver nenhum registro na tabela, ele retornaria nulo.
