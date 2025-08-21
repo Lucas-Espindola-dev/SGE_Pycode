86de572e097889694b485fc3a707ae9e8719bb3e
Lucas Espindola <lucasespindola.dev@gmail.com>
2025-08-21 10:37:35 -0300

    Corrections

--- brands/urls.py
+++ brands/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 
-urlspatterns = [
-    path('brands/list', views.BrandListView.as_view(), name='brands_list')
+urlpatterns = [
+    path('brands/list/', views.BrandListView.as_view(), name='brands_list')
 ]
