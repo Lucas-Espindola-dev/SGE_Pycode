e49ffa3076dcd6f4ac3dcf31c1612211d65e140b
Lucas Espindola <lucasespindola.dev@gmail.com>
2025-08-26 16:58:28 -0300

    Creating the path to a create brands page

--- brands/urls.py
+++ brands/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('brands/list/', views.BrandListView.as_view(), name='brands_list')
+    path('brands/list/', views.BrandListView.as_view(), name='brands_list'),
+    path('brands/list/', views.CreateView.as_view(), name='brands_create'),
 ]
