8880eda592080b94a894ded88a457ceec820c079
Lucas Espindola <lucasespindola.dev@gmail.com>
2025-09-02 17:49:46 -0300

    Correcting description

--- categories/models.py
+++ categories/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=256)
-    descrition = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
